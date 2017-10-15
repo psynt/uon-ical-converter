e0d2fa204fad213f11a3e227fffda850d7eba4e2
nichita <nichita@cadvegra.com>
2017-10-15 23:57:47 +0100

    changed starting date to always be today

--- src/main/groovy/ical/ICalGen.groovy
+++ src/main/groovy/ical/ICalGen.groovy
@@ -13,6 +13,7 @@ class ICalGen {
     }
 
     def generateEvents(){
+        def kyou = new Date().format( 'yyyyMMdd' )
 //        prodid('-//Ben Fortuna//iCal4j 1.0//EN')
 //        version('2.0')
         inp.each{ List<String> entry ->
@@ -38,12 +39,12 @@ class ICalGen {
 
             def str =
 """BEGIN:VEVENT
-DTSTART;TZID=Europe/London:20170925T${beg}00
-DTEND;TZID=Europe/London:20170925T${end}00
+DTSTART;TZID=Europe/London:${kyou}T${beg}00
+DTEND;TZID=Europe/London:${kyou}T${end}00
 RRULE:FREQ=WEEKLY;COUNT=2;BYDAY=${entry[2].substring(0,2).toUpperCase()}
-DTSTAMP:20170925T164430Z
+DTSTAMP:${kyou}5T164430Z
 UID:${RandomUtils.nextInt()}
-CREATED:20170925T163941Z
+CREATED:${kyou}5T163941Z
 LOCATION:${entry[5]}
 SUMMARY:${entry[0].substring(0,6)}
 END:VEVENT"""
@@ -53,3 +54,7 @@ END:VEVENT"""
     }
 
 }
+
+
+
+
